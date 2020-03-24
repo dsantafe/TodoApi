5a47fc4088e95e4b545bf0dcf870c758f7473a83
David Santafe <santafedavid01@gmail.com>
2020-03-23 22:17:41 -0500

    Agregar archivos de proyecto.

---
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -0,0 +1,121 @@
+﻿using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [RoutePrefix("api/TodoItems")]
+    public class TodoItemsController : ApiController
+    {
+        private readonly TodoContext _context;
+
+        public TodoItemsController()
+        {
+            _context = new TodoContext();
+        }
+
+        // POST: api/TodoItems
+        [HttpPost]
+        public async Task<IHttpActionResult> PostTodoItem(TodoItemDTO todoItemDTO)
+        {
+            var todoItem = new TodoItem
+            {
+                IsComplete = todoItemDTO.IsComplete,
+                Name = todoItemDTO.Name
+            };
+
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return Ok(ItemToDTO(todoItem));
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetTodoItems()
+        {
+            var result = _context.TodoItems
+                    .Select(x => x)
+                    .ToList();
+
+            var resultDTO = result.Select(x => ItemToDTO(x)).ToList();
+
+            return Ok(resultDTO);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetTodoItem(long id)
+        {
+            var todoItem = _context.TodoItems.Find(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            var resultDTO = ItemToDTO(todoItem);
+
+            return Ok(resultDTO);
+        }
+
+        private static TodoItemDTO ItemToDTO(TodoItem todoItem) => new TodoItemDTO
+        {
+            Id = todoItem.Id,
+            Name = todoItem.Name,
+            IsComplete = todoItem.IsComplete
+        };
+
+        // PUT: api/TodoItems/5
+        [HttpPut]
+        public async Task<IHttpActionResult> PutTodoItem(long id, TodoItemDTO todoItemDTO)
+        {
+            if (id != todoItemDTO.Id)
+            {
+                return BadRequest();
+            }
+
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            todoItem.Name = todoItemDTO.Name;
+            todoItem.IsComplete = todoItemDTO.IsComplete;
+            
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
+            {
+                return NotFound();
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        private bool TodoItemExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext() : base("TodoContext")
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
